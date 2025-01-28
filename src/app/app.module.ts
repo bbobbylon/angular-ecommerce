583d63c0c197bd0a10b314c77e835e3afb2839a5
bobby <robertcoliver@lewisu.edu>
2025-01-27 21:58:47 -0500

    CSS changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
-import {HttpClientModule} from '@angular/common/http';
 import {ProductService} from './services/product.service';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
+    HttpClientModule
+
 
   ],
   providers: [ProductService],
--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/product.service';
 
+
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list-table.component.html',
