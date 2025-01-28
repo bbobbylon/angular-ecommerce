f1266dbfdc5d2cba110a17578d2b91e049c85713
bobby <robertcoliver@lewisu.edu>
2025-01-27 22:52:28 -0500

    Adding some console logs for clear view of the data being sent

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -5,7 +5,8 @@ import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
-  templateUrl: './product-list-table.component.html',
+  templateUrl: './product-list-grid.component.html',
+  //templateUrl: './product-list-table.component.html',
   //templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
@@ -20,10 +21,21 @@ export class ProductListComponent implements OnInit {
     this.listProducts();
   }
   listProducts() {
-    //this method is invoked once you 'subscribe' to the observable
+    //this method is invoked once you 'subscribe' to the observable, and it populates the products array with the data from the sql database
     this.productService.getProductList().subscribe(data => {
       //asign the results to the products array
       this.products = data;
+      //some logs to see the data clearly in the console
+      //to see each book in the array
+      this.products.forEach(book => {
+        console.log(book);
+      });
+      //to see each book as an array
+      console.log(this.products);
+      //to see the first book's cost
+      console.log(this.products[0].unitPrice);
+      //to see all of the books as an array
+      console.log(data);
     }); 
   }
 
