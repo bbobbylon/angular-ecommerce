67b851ebb93f52dda897333ade076b03e2a57148
bobby <robertcoliver@lewisu.edu>
2025-02-15 18:11:55 -0500

    Adding products stored in our database

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -19,19 +19,33 @@ export class ProductListComponent implements OnInit {
   //similar to our @POSTConstruct
   ngOnInit(): void {
     this.listProducts();
+    this.listProductsFromSpecificPage();
   }
+
+  listProductsFromSpecificPage() {  
+    this.productService.getProductListFromSpecificPage(1, 5).subscribe(data => {
+      this.products = data;
+      console.log('Product List:', this.products);
+      this.products.forEach(book => {
+        console.log(book);
+      });
+      console.log(this.products[0].unitPrice);
+      console.log(data);
+    });
+  }
+
   listProducts() {
     //this method is invoked once you 'subscribe' to the observable, and it populates the products array with the data from the sql database
     this.productService.getProductList().subscribe(data => {
       //asign the results to the products array
       this.products = data;
+       console.log('Product List:', this.products); // Log the product list as an array
       //some logs to see the data clearly in the console
       //to see each book in the array
       this.products.forEach(book => {
         console.log(book);
       });
-      //to see each book as an array
-      console.log(this.products);
+
       //to see the first book's cost
       console.log(this.products[0].unitPrice);
       //to see all of the books as an array
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -8,7 +8,7 @@ import { Product } from '../common/product';
 })
 export class ProductService {
 
-  private baseUrl = 'http://localhost:8080/api/products?size=100';
+  private baseUrl = 'http://localhost:8080/api/products';
 
   constructor(private httpClient: HttpClient) { }
 
@@ -19,11 +19,33 @@ export class ProductService {
       map(response => response._embedded.products)
     );
   }
+    // Fetch products from a specific page
+    getProductListFromSpecificPage(page: number = 0, size: number = 20): Observable<Product[]> {
+      const url = `${this.baseUrl}?page=${page}&size=${size}`;
+      return this.httpClient.get<GetResponse>(url).pipe(
+        map(response => response._embedded.products)
+      );
+    }
+
+
+    //get all of the data and display on a single page
+  getAllProducts(): Observable<number> {
+    return this.httpClient.get<GetResponse>(`${this.baseUrl}?size=1`).pipe(
+      map(response => response.page.totalElements)
+    );
+  }
 }
+
+
+
+  
 
 //this will unwrap the json data from spring Data Rest _embedded entry that comes back from the REST api, and will access the array of Products
 interface GetResponse {
   _embedded: {
     products: Product[];
+  },
+  page: {
+    totalElements: number;
   }
 }
