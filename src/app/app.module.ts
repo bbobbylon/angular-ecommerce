20bd24fdd9ce636ed4a2578e14a486ece402c076
bobby <robertcoliver@lewisu.edu>
2025-02-15 18:19:23 -0500

    Defining our routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,24 @@ import { ProductListComponent } from './components/product-list/product-list.com
 import { ProductService } from './services/product.service';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { Router, RouterModule, Routes } from '@angular/router';
+//when the path is matched, we will create a new intance of the component referenced
 
+const routes: Routes = [
+  {path: 'category/:id', component: ProductListComponent},
+  {path: 'category', component: ProductListComponent},
+  {path: 'products', component: ProductListComponent},
+  {path: '', redirectTo: '/products', pathMatch: 'full'},
+  //generic wildcard that will match anything that is not explicitly mentioned above
+  {path: '**', redirectTo: '/products', pathMatch: 'full'}
+];
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     HttpClientModule,
     CommonModule
